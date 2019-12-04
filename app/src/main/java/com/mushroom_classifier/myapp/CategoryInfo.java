847961b0f8474239f1323d5d3efbeaa8693953b8
Kuangyuan Sun <ksun6@hotmail.com>
2019-12-03 23:06:13 -0600

    first implement

---
+++ app/src/main/java/com/mushroom_classifier/myapp/CategoryInfo.java
@@ -0,0 +1,9 @@
+package com.mushroom_classifier.myapp;
+
+
+public class CategoryInfo {
+    protected Integer cat_id;
+    protected String cat_name;
+    protected String info;
+    protected byte[] image;
+}
